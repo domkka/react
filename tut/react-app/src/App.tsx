da95389c72b84314645598da4cae7e3e037354b5
Dom <domkkaaaa@gmail.com>
2024-10-01 16:46:03 +0200

    react basic

---
+++ tut/react-app/src/App.tsx
@@ -0,0 +1,25 @@
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
